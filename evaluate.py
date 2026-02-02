6201ff03fa5f4771d21d64511cf9e9005de2955a
gpriya24 <gpriya2409@gmail.com>
2026-02-02 09:20:33 +0530

    evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,26 @@
 import torch
 from metrics import rmse, mae, mape
 
-def evaluate_model(model, X_test, y_test):
+def evaluate_transformer(model, X_test, y_test, scaler):
     model.eval()
     X_test = torch.tensor(X_test, dtype=torch.float32)
-    preds = model(X_test).detach().numpy()
 
-    print("RMSE:", rmse(y_test, preds))
-    print("MAE:", mae(y_test, preds))
-    print("MAPE:", mape(y_test, preds))
+    with torch.no_grad():
+        preds = model(X_test, X_test).numpy()
+
+    preds_inv = scaler.inverse_transform(preds)
+    y_test_inv = scaler.inverse_transform(y_test)
+
+    return {
+        "RMSE": rmse(y_test_inv, preds_inv),
+        "MAE": mae(y_test_inv, preds_inv),
+        "MAPE": mape(y_test_inv, preds_inv)
+    }
+
+def evaluate_sarima(sarima_model, test_series):
+    forecast = sarima_model.forecast(len(test_series))
+    return {
+        "RMSE": rmse(test_series, forecast),
+        "MAE": mae(test_series, forecast),
+        "MAPE": mape(test_series, forecast)
+    }
